accd9a7e04df4ae537bae599562c84803c36d87b
harel <harelyishay@gmail.com>
2023-04-13 17:27:25 +0300

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+
+extern crate nalgebra as na;
+
+use na::{Vector6, Matrix6};
+
+
 fn main() {
-    println!("Hello, world!");
+    let t = Matrix6::<i32>::new(0, 1, 0, 0, 0, 0,
+                                0, 0, 0, 0, 0, 1,
+                                0, 0, 1, 0, 0, 0,
+                                1, 0, 0, 0, 0, 0,
+                                0, 0, 0, 0, 1, 0,
+                                0, 0, 0, 1, 0, 0,);
+
+
+    let c = Vector6::new(0,1,2,3,4,5);
+
+
+    let r = t * c;
+    println!("{}", t);
+    println!("{}", c);
+    println!("{}", r);
 }

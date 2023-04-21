5292e05b6a52a49e4056fea9a517e7939f6eaefa
harel <harelyishay@gmail.com>
2023-04-21 13:02:48 +0300

    added ONE test

--- src/m_gen.rs
+++ src/m_gen.rs
@@ -1,16 +1,17 @@
 
 
-use na::{SMatrix, U6, SVector, Dim, Const, DimName, Vector, Storage, U1};
+use na::{ArrayStorage, U6, DVector, Dim, Const, DimName, Vector, Storage, U1};
 //use num_derive::FromPrimitive;
 
 
 
 
-/*fn add_vectors<N: DimName>(
-    a: &Vector<i32, N, dyn Storage<i32, N, U1>>, 
-    b: &Vector<i32, N, dyn Storage<i32, N, U1, CStride = i32, RStride = i32>>)
-     -> Vector<i32, N, dyn Storage<i32, N, U1, CStride = i32, RStride = i32>> {
-    let m: usize = 3;
-    a + b + SVector::<f64, N>::repeat(1.0) * m as f64
-}*/
+
+fn addv(a: &DVector<i32>, b: &DVector<i32>) -> DVector<i32>
+{
+    assert_eq!(a.len(), b.len());
+    a + b
+}
+
+
 
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 
 extern crate nalgebra as na;
 
-//use na::{Vector6, Matrix6};
+use na::{DVector};
 
 mod colors;
 
@@ -14,32 +14,8 @@ use cube111::{Cube111};
 mod m_gen;
 
 fn main() {
-    // let t = Matrix6::<i32>::new(0, 1, 0, 0, 0, 0,
-    //                             0, 0, 0, 0, 0, 1,
-    //                             0, 0, 1, 0, 0, 0,
-    //                             1, 0, 0, 0, 0, 0,
-    //                             0, 0, 0, 0, 1, 0,
-    //                             0, 0, 0, 1, 0, 0,);
 
-
-    //let c = Vector6::new(C::W,C::B,C::O,C::G,C::R,C::Y);
-
-
-    //let r = (t * c.to_numerical()).to_colorical();
-    //println!("{}", t);
-    //!("{}", c);
-    //println!("{}", r);
-    assert!(Cube111::new().x().mx() == Cube111::new());
-    assert!(Cube111::new().x().x().x().x() == Cube111::new());
-    assert!(Cube111::new().x().x() == Cube111::new().mx().mx());
-    assert_eq!(Cube111::new().x().y().z(), Cube111::new().x().x().y());
-    assert_eq!(Cube111::new(), Cube111::new()
-    .x().x().x().x()
-    .mx().mx().mx().mx()
-    .y().y().y().y()
-    .my().my().my().my()
-    .z().z().z().z()
-    .mz().mz().mz().mz());
+    
 
 
     let c1 = Cube111::new();
@@ -53,3 +29,35 @@ fn main() {
 
 
 }
+
+#[cfg(test)]
+pub mod tests
+{
+    use super::*;
+
+    #[test]
+    pub fn cube111_test()
+    {
+        assert!(Cube111::new().x().mx() == Cube111::new());
+        assert!(Cube111::new().x().x().x().x() == Cube111::new());
+        assert!(Cube111::new().x().x() == Cube111::new().mx().mx());
+        assert_eq!(Cube111::new().x().y().z(), Cube111::new().x().x().y());
+        assert_eq!(Cube111::new(), Cube111::new()
+        .x().x().x().x()
+        .mx().mx().mx().mx()
+        .y().y().y().y()
+        .my().my().my().my()
+        .z().z().z().z()
+        .mz().mz().mz().mz());
+    }
+
+    #[test]
+    pub fn m_gen_test()
+    {
+        //let _ = m_gen::addv();
+    }
+
+}
+
+
+

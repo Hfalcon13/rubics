2f2e47c4de54c5d1adda17b7853ae62c2e969814
harel <harelyishay@gmail.com>
2023-04-24 21:49:23 +0300

    complite mot thoroly tested mgen

--- src/m_gen.rs
+++ src/m_gen.rs
@@ -11,15 +11,17 @@ fn index_of(v: &DVector<i32>, e: i32) -> usize
 
 fn my_apply<F>(v: &DVector<i32>, f: F) -> DVector<i32>
 where
-    F: Fn(i32) -> i32,
+    F: Fn(i32, usize) -> i32,
 {
     let mut result: DVector<i32> = DVector::zeros(v.len());
     for i in 0..v.len()
     {
-        result[i] = f(v[i]);
+        result[i] = f(v[i], i);
     }
     result
 }
+
+
 
 pub fn mgen(a: &DVector<i32>, b: &DVector<i32>) -> DMatrix<i32>
 {
@@ -27,13 +29,14 @@ pub fn mgen(a: &DVector<i32>, b: &DVector<i32>) -> DMatrix<i32>
     let mut result: DMatrix<i32> = DMatrix::zeros(a.len(), a.len());
     for i in 0..a.len()
     {
+        println!("mgen {} {} {}", i, b[i], index_of(a, b[i]));
         result
         .set_row(i, 
             &my_apply(&DVector::<i32>::zeros(a.len()), 
-            |_|if i == index_of(a, b[i]){1}else{0})
+            |_,j|if j == index_of(a, b[i]){1}else{0})
         .transpose());
     }
-
+    
     result
 }
 
